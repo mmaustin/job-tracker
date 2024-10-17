6b142868f3ff9e35cef64f49e420e93b94a85503
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-17 17:20:56 -0400

    Created navlinks type and exported links array

---
+++ tracker/utils/links.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {AreaChart, Layers, AppWindow} from 'lucide-react';
+
+type NavLink = {
+  href: string,
+  label: string,
+  icon: React.ReactNode
+};
+
+export const links: NavLink[] = [
+  {
+    href: '/add-job',
+    label: 'add job',
+    icon: <Layers />,
+  },
+  {
+    href: '/jobs',
+    label: 'jobs',
+    icon: <AppWindow />,
+  },
+  {
+    href: '/stats',
+    label: 'stats',
+    icon: <AreaChart />,
+  }
+];
