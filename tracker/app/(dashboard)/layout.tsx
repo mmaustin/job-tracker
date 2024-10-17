494586a7965dc07e3ca79289be8c5b54137f3abe
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-17 18:00:59 -0400

    Set up general layout for the dashboard

--- tracker/app/(dashboard)/layout.tsx
+++ tracker/app/(dashboard)/layout.tsx
@@ -1,8 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+import { PropsWithChildren } from 'react';
 
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const layout = ({ children }: PropsWithChildren) => {
   return (
-    <div>{children}</div>
+    <main className='grid md:grid-cols-5'>
+      <div className="hidden md:block md:col-span-1 md:min-h-screen">
+        <Sidebar />
+      </div>
+      <div className="md:col-span-4">
+        <Navbar />
+        <div className="py-16 px-4 sm:px-8 md:px-16">
+          {children}
+        </div>
+      </div>
+    </main>
   )
 }
 export default layout;
