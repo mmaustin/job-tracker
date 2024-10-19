3839ae8b1f52c09174dbaf0493e0d210ba9cb2b5
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-19 10:35:52 -0400

    Completed initial sidebar setup

--- tracker/components/Sidebar.tsx
+++ tracker/components/Sidebar.tsx
@@ -6,13 +6,25 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { usePathname } from "next/navigation";
-import {TbLetterJ} from 'react-icons/tb'
+import { TbLetterJ } from 'react-icons/tb'
 
 
 const Sidebar = () => {
+
+  const pathname = usePathname();
+
   return (
     <aside className="py-4 px-8 bg-muted h-full">
       <TbLetterJ className="mx-auto" />
+      <div className="flex flex-col mt-20 gap-y-4">
+        {links.map(link => {
+          return <Button asChild key={link.href} variant={pathname === link.href ? 'default' : 'link'}>
+            <Link href={link.href} className="flex items-center gap-x-2">
+              {link.icon} <span className="capitalize">{link.label}</span>
+            </Link>
+          </Button>
+        })}
+      </div>
     </aside>
   )
 }
