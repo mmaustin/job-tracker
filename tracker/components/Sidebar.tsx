f545135c7d0297c36793ed5573eedfb6b0d52659
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-19 10:26:29 -0400

    Imported assets into sidebar component

--- tracker/components/Sidebar.tsx
+++ tracker/components/Sidebar.tsx
@@ -1,9 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client'
 
+import { links } from "@/utils/links";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
+import {TbLetterJ} from 'react-icons/tb'
 
 
 const Sidebar = () => {
   return (
-    <h2 className="text-3xl">Side bar</h2>
+    <aside className="py-4 px-8 bg-muted h-full">
+      <TbLetterJ className="mx-auto" />
+    </aside>
   )
 }
 export default Sidebar;
