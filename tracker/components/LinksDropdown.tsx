6174fdb4090b9d8178c56bc55843d06b9b09ca97
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-20 12:42:52 -0400

    Changed themeing color with shadcn/themes

--- tracker/components/LinksDropdown.tsx
+++ tracker/components/LinksDropdown.tsx
@@ -19,7 +19,7 @@ const LinksDropdown = () => {
           return (
             <DropdownMenuItem key={link.href}>
               <Link href={link.href} className="flex items-center gap-x-2">
-              {link.icon} <span className="capitalize">{link.label}</span>
+                {link.icon} <span className="capitalize">{link.label}</span>
               </Link>
             </DropdownMenuItem>
           )
