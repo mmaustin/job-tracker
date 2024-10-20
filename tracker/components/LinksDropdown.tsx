c861eacdf5be0db87ff1b8895462be8cff9d1e5a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-20 12:12:23 -0400

    Set up the dropdown trigger via shadcn

--- tracker/components/LinksDropdown.tsx
+++ tracker/components/LinksDropdown.tsx
@@ -1,9 +1,19 @@
-
-
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { AlignLeft } from 'lucide-react';
 
 const LinksDropdown = () => {
   return (
-    <h2 className="text-3xl">Links Dropdown</h2>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="md:hidden">
+        <Button variant={'outline'} size={'icon'}>
+          <AlignLeft />
+          <span className="sr-only">Toggle Links</span>
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
   )
 }
 export default LinksDropdown;
