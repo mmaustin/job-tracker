24cbe92e10a9aa180a659939654809e29f43e1c9
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-24 18:09:04 -0400

    Created custom form select component and component props

--- tracker/components/FormComponents.tsx
+++ tracker/components/FormComponents.tsx
@@ -40,3 +40,47 @@ export function CustomFormField({ name, control }: CustomFormFieldProps) {
     />
   );
 };
+
+type CustomFormSelectProps = {
+  name: string;
+  control: Control<any>;
+  items: string[];
+  labelText?: string;
+};
+
+export function CustomFormSelect({
+  name,
+  control,
+  items,
+  labelText,
+}: CustomFormSelectProps) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className='capitalize'>{labelText || name}</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {items.map((item) => {
+                return (
+                  <SelectItem key={item} value={item}>
+                    {item}
+                  </SelectItem>
+                );
+              })}
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
+export default CustomFormSelect;
