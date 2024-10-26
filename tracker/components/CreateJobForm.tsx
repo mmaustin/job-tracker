c699c9511013dd6a1086b89a4c1609b390165aab
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-26 15:54:29 -0400

    Added customform field and select to return tsx

--- tracker/components/CreateJobForm.tsx
+++ tracker/components/CreateJobForm.tsx
@@ -26,7 +26,27 @@ const CreateJobForm = () => {
   };
 
   return (
-    <div>CreateJobForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='bg-muted p-8 rounded'>
+        <h2 className="capitalize font-semibold text-4xl mb-6">
+          add job
+        </h2>
+        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 items-start">
+          <CustomFormField name='position' control={form.control} />
+          <CustomFormField name='company' control={form.control} />
+          <CustomFormField name='location' control={form.control} />
+
+          <CustomFormSelect name='status' control={form.control} labelText='job status' items={Object.values(JobStatus)}
+          />
+          <CustomFormSelect name='mode' control={form.control} labelText='job mode' items={Object.values(JobMode)}
+          />
+
+          <Button type='submit' className='self-end capitalize'>
+            create job
+          </Button>
+        </div>
+      </form>
+    </Form>
   )
 }
 export default CreateJobForm;
