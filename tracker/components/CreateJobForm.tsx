80429e5ae74c195996f335d91636c444128c7ffd
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-26 14:50:48 -0400

    Imported type, form, and schema assets to refactor create job form

--- tracker/components/CreateJobForm.tsx
+++ tracker/components/CreateJobForm.tsx
@@ -1,63 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { JobStatus, JobMode, createAndEditJobSchema, CreateAndEditJobType } from '@/utils/types';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Button } from './ui/button';
+import { Form } from './ui/form';
+import { CustomFormField, CustomFormSelect } from './FormComponents';
 
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
-
-export function CreateJobForm() {
-  
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: '',
-    }
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>):void{
-    console.log(values);
-    
-  }
-
+const CreateJobForm = () => {
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription className="capitalize">
-                shadcn is more complicated than next!
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div>CreateJobForm</div>
   )
-};
+}
 export default CreateJobForm;
