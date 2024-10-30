464ac38261e943c745bf15e608c6770e49f44489
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-30 16:16:08 -0400

    Set up react query in createjobsform component

--- tracker/components/CreateJobForm.tsx
+++ tracker/components/CreateJobForm.tsx
@@ -7,6 +7,10 @@ import { useForm } from 'react-hook-form';
 import { Button } from './ui/button';
 import { Form } from './ui/form';
 import { CustomFormField, CustomFormSelect } from './FormComponents';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createJobAction } from '@/utils/actions';
+import { useToast } from '@/hooks/use-toast';
+import { useRouter } from 'next/router';
 
 
 const CreateJobForm = () => {
@@ -22,8 +26,28 @@ const CreateJobForm = () => {
     },
   });
 
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (values: CreateAndEditJobType) => createJobAction(values),
+    onSuccess: data => {
+      if (!data) {
+        toast({ description: 'there was an error' });
+        return;
+      };
+      toast({ description: 'job created' });
+      queryClient.invalidateQueries({ queryKey: ['jobs'] });
+      queryClient.invalidateQueries({ queryKey: ['stats'] });
+      queryClient.invalidateQueries({ queryKey: ['charts'] });
+
+      router.push('/jobs');
+    }
+  })
+
   function onSubmit(values: CreateAndEditJobType) {
-    console.log(values);
+    mutate(values);
   };
 
   return (
@@ -42,8 +66,8 @@ const CreateJobForm = () => {
           <CustomFormSelect name='mode' control={form.control} labelText='job mode' items={Object.values(JobMode)}
           />
 
-          <Button type='submit' className='self-end capitalize'>
-            create job
+          <Button type='submit' className='self-end capitalize' disabled={isPending}>
+            {isPending ? 'loading' : 'create job'}
           </Button>
         </div>
       </form>
