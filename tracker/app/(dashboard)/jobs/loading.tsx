559f372e4d24b0218225f158df5bd6ac509fcb9b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-11-06 16:48:50 -0500

    Imported search components to job page

---
+++ tracker/app/(dashboard)/jobs/loading.tsx
@@ -0,0 +1,8 @@
+
+
+function loading() {
+  return (
+    <h2 className="text-xl font-medium capitalize" >loading . . . </h2>
+  )
+}
+export default loading
--- tracker/app/(dashboard)/jobs/page.tsx
+++ tracker/app/(dashboard)/jobs/page.tsx
@@ -1,8 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import JobsList from "@/components/JobsList";
+import SearchForm from "@/components/SearchForm";
+import { getAllJobsAction } from "@/utils/actions";
+import { dehydrate, QueryClient, HydrationBoundary } from "@tanstack/react-query";
 
 
-const JobsPage = () => {
+const JobsPage = async () => {
+
+  const queryClient = new QueryClient();
+  const gotJobs = await queryClient.prefetchQuery({
+    queryKey: ['jobs', '', 'all', 1],
+    queryFn: () => getAllJobsAction({}),
+  });
+  console.log(gotJobs);
+  
+
   return (
-    <div>Jobs Page</div>
+    <HydrationBoundary state={dehydrate(queryClient)} >
+      <SearchForm />
+      <JobsList />
+    </HydrationBoundary>
   )
 }
 export default JobsPage;
