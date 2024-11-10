4432f9fb10302c31188306dcd55db6e0c8a56e1b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-11-10 15:04:49 -0500

    Set up initial, non-functional tsx

--- tracker/components/SearchForm.tsx
+++ tracker/components/SearchForm.tsx
@@ -12,8 +12,23 @@ function SearchForm() {
 
 
   return (
-    <form className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg">
-      
+    <form onSubmit={} className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg">
+      <Input type="text" placeholder="Search Jobs" name="search" />
+      <Select>
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+        {
+          ['all', ...Object.values(JobStatus)].map(jobStatus => {
+            return <SelectItem key={jobStatus} value={jobStatus}>
+              {jobStatus}
+            </SelectItem>
+          })
+        }
+        </SelectContent>
+      </Select>
+      <Button type="submit" >Search</Button>
     </form>
   )
 }
