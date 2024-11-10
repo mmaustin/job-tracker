48543fa85e96623d5fbcc87a458092c586ebaca7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-11-10 14:55:30 -0500

    Imported need hooks, types, and components to search form component

--- tracker/components/SearchForm.tsx
+++ tracker/components/SearchForm.tsx
@@ -1,8 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client'
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { JobStatus } from "@/utils/types";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 
 function SearchForm() {
+
+
+
   return (
-    <div>SearchForm</div>
+    <form className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg">
+      
+    </form>
   )
 }
 export default SearchForm;
