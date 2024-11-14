a3e4aefd62a327c3b8f56b3212c7ed5debf179b6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-11-14 17:42:43 -0500

    Imports to jobcard component

--- tracker/components/JobCard.tsx
+++ tracker/components/JobCard.tsx
@@ -1,8 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {JobType} from '@/utils/types';
+import Link from 'next/link';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import { Badge } from './ui/badge';
+import JobInfo from './JobInfo';
+import DeleteJobBtn from './DeleteJobBtn';
 
 
 function JobCard({job}: {job: JobType}) {
+  const date = new Date(job.createdAt).toLocaleDateString();
 
   return (
     <div className='capitalize'>{job.location}</div>
--- tracker/components/JobsList.tsx
+++ tracker/components/JobsList.tsx
@@ -3,7 +3,7 @@
 
 import { getAllJobsAction } from "@/utils/actions";
 import { useQuery } from "@tanstack/react-query";
-import {JobType} from '@/utils/types';
+import { JobType } from '@/utils/types';
 import JobCard from "./JobCard";
 import { useSearchParams } from "next/navigation";
 
@@ -16,32 +16,34 @@ function JobsList() {
 
   const pageNumber = Number(searchParams.get('page')) || 1;
 
-  const {data, isPending} = useQuery({
+  const { data, isPending } = useQuery({
     queryKey: ['jobs', search, jobStatus, pageNumber],
-    queryFn: () => getAllJobsAction({search, jobStatus, page: pageNumber}),
+    queryFn: () => getAllJobsAction({ search, jobStatus, page: pageNumber }),
   })
 
   let parsedJobs: JobType[] | undefined;
-  if(data){
+  if (data) {
     parsedJobs = data.jobs.map(job => {
       return JSON.parse(job);
     });
-  };
+  } else {
+    parsedJobs = undefined
+  }
 
-  if(isPending) return <h2 className="text-xl">Please Wait . . . </h2>
-  if(!parsedJobs) return <h2 className="text-xl">No Jobs Found . . . </h2>
-  
+  if (isPending) return <h2 className="text-xl">Please Wait . . . </h2>
+  if (!parsedJobs) return <h2 className="text-xl">No Jobs Found . . . </h2>
+
   return (
     <>
-    <div className="grid md:grid-cols-2 gap-8">
-      {
-        // parsedJobs ?
-        parsedJobs.map((job)=>{
-          return <JobCard key={job._id} job={job} />
-        }) 
-        // : <h2 className="text-xl">No Jobs Found . . . </h2>
-      }
-    </div>
+      <div className="grid md:grid-cols-2 gap-8">
+        {
+          // parsedJobs ?
+          parsedJobs.map((job) => {
+            return <JobCard key={job._id} job={job} />
+          })
+          // : <h2 className="text-xl">No Jobs Found . . . </h2>
+        }
+      </div>
     </>
   )
 }
