b92517158f18480a7844145b6c2127b46c81f697
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-11-23 10:03:22 -0500

    Looaded prefetched stats data in stats container

--- tracker/components/StatsContainer.tsx
+++ tracker/components/StatsContainer.tsx
@@ -1,6 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
+import { getStatsAction } from "@/utils/actions";
+import { useQuery } from "@tanstack/react-query";
+import StatsCard from "./StatsCard";
+
 function StatsContainer() {
+  const {data} = useQuery({
+    queryKey: ['stats'],
+    queryFn: () => getStatsAction()
+  });
+
+  console.log(data);
+  
+
   return (
     <div>StatsContainer</div>
   )
