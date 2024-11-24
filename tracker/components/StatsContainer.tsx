3adb9ec0856aa16a29880d7a55e79d1ac5db4692
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-11-24 10:19:15 -0500

    Added skeleton components to stats and jobs loading pages

--- tracker/components/StatsContainer.tsx
+++ tracker/components/StatsContainer.tsx
@@ -3,14 +3,22 @@
 
 import { getStatsAction } from "@/utils/actions";
 import { useQuery } from "@tanstack/react-query";
-import StatsCard from "./StatsCard";
+import StatsCard, { StatsLoadingCard } from "./StatsCard";
 
 function StatsContainer() {
-  const {data} = useQuery({
+  const { data, isPending } = useQuery({
     queryKey: ['stats'],
     queryFn: () => getStatsAction()
   });
 
+  if (isPending)
+    return (
+      <div className='grid md:grid-cols-2 gap-4 lg:grid-cols-3'>
+        <StatsLoadingCard />
+        <StatsLoadingCard />
+        <StatsLoadingCard />
+      </div>
+    );
 
   return (
     <div className="grid md:grid-cols-2 gap-4 lg:grid-cols-3">
