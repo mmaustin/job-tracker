8a675785a40ab7ac08b61b2cd58810ef1af3ed8a
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-12-16 14:59:30 -0500

    Added styling to the error page

--- tracker/app/(dashboard)/error.tsx
+++ tracker/app/(dashboard)/error.tsx
@@ -1,4 +1,7 @@
 'use client'
+
+import { Button } from "@/components/ui/button"
+
  
 export default function Error({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -9,9 +12,9 @@ export default function Error({
   reset: () => void
 }) {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button onClick={() => reset()}>Try again</button>
+    <div className="flex flex-col justify-center items-center">
+      <h2 className="mb-2 font-serif text-xl">Something went wrong!</h2>
+      <Button className="bg-blue-500" variant="secondary" size="default" onClick={() => reset()}>Try again</Button>
     </div>
   )
 }
