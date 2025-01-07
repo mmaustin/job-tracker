5dbe7b85609ad2f6162a756d4e103bc0774a2abc
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-01-07 17:26:26 -0500

    Added job-card pic to homepage

--- tracker/app/page.tsx
+++ tracker/app/page.tsx
@@ -1,18 +1,25 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-//import Image from "next/image";
+import Image from "next/image";
+import jobCard from "@/public/job-card.png"
 
 
 
 export default function Home() {
   return (
-        <div className="mx-5 flex flex-col justify-center items-center">
-      {/* <div className="my-10 w-full flex justify-center items-center">
-        <div className="w-full flex justify-between items-center">
-          <ThemeToggle />
-        </div>
-      </div> */}
-      <div className="mt-32 flex justify-center items-center">
+    <div className="mx-5 flex flex-col justify-center items-center">
+      <div className="rounded-lg mt-10">
+        {/* <h2 className="text-xs font-serif"></h2> */}
+        <Image
+          className="h-[200px] w-[300px] object-contain border border-base-300/50 hover:border-neutral-800 rounded-lg bg-black"
+          src={jobCard}
+          alt="create author profile form"
+          priority
+          height={265}
+          width={198}
+        />
+      </div>
+      <div className="mt-10 flex justify-center items-center">
         <div className="max-w-3xl flex flex-col justify-center items-center">
           <h1 className="text-3xl md:text-5xl font-bold uppercase border-t-2 border-dotted text-center">
             Just One
@@ -20,10 +27,10 @@ export default function Home() {
           </h1>
           <div className="flex flex-wrap justify-center items-center">
             <Button size={'lg'} asChild>
-        <Link href={'/add-job'} >
-          Start Tracking
-        </Link>
-      </Button>
+              <Link href={'/add-job'} >
+                Start Tracking
+              </Link>
+            </Button>
             {/* <div className="tooltip tooltip-bottom mt-10" data-tip="Outlook Is Not Recommended. May Experience Redirect Issues On Login.">
               <IoInformationCircle className="m-0" />
             </div> */}
@@ -31,12 +38,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-    // <main className="display flex justify-center items-center h-[300px]">
-    //   <Button size={'lg'} asChild>
-    //     <Link href={'/add-job'} >
-    //       Get Started
-    //     </Link>
-    //   </Button>
-    // </main>
   );
 };
