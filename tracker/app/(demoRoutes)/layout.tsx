2eca9e1c53a71e3c2fa5e2e474f5a8c77decc77c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-28 17:48:43 -0400

    Added a layout file to demoRoutes folder

---
+++ tracker/app/(demoRoutes)/layout.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from 'react';
+
+
+const layout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className='grid md:grid-cols-5'>
+      <div className="hidden md:block md:col-span-1 md:min-h-screen">
+        <p>Sidebar</p>
+        {/* <Sidebar /> */}
+      </div>
+      <div className="md:col-span-4">
+        <p>Navbar</p>
+        {/* <Navbar /> */}
+        <div className="py-16 px-4 sm:px-8 md:px-16">
+          {children}
+        </div>
+      </div>
+    </main>
+  )
+}
+export default layout;
