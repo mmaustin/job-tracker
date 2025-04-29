818f413f12d44c67f5bb83b6727948e150017e4c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-29 17:09:07 -0400

    Created a demo links dropdown component

---
+++ tracker/components/demoComponents/DemoLinksDropdown.tsx
@@ -0,0 +1,31 @@
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { AlignLeft } from 'lucide-react';
+import { demoLinks } from "@/utils/demoLinks";
+
+const LinksDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="md:hidden">
+        <Button variant={'outline'} size={'icon'}>
+          <AlignLeft />
+          <span className="sr-only">Toggle Links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-52 md:hidden" align="start" sideOffset={25} >
+        {demoLinks.map(link => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className="flex items-center gap-x-2">
+                {link.icon} <span className="capitalize">{link.label}</span>
+              </Link>
+            </DropdownMenuItem>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+export default LinksDropdown;
