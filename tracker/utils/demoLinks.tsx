78d102831fe7812303381a7ef473d9059acaea31
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-04-29 16:55:36 -0400

    Created a demoLinks file with routes array

---
+++ tracker/utils/demoLinks.tsx
@@ -0,0 +1,25 @@
+import { AreaChart, Layers, AppWindow } from 'lucide-react';
+
+type NavLink = {
+  href: string,
+  label: string,
+  icon: React.ReactNode
+};
+
+export const demoLinks: NavLink[] = [
+  {
+    href: '/demo-add-job',
+    label: 'add job',
+    icon: <Layers />,
+  },
+  {
+    href: '/demo-jobs',
+    label: 'jobs',
+    icon: <AppWindow />,
+  },
+  {
+    href: '/demo-stats',
+    label: 'stats',
+    icon: <AreaChart />,
+  }
+];
