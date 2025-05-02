9fe99741c8c6b03c0af3ab1a34d52481aef4d384
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-02 17:07:14 -0400

    Placed under construction react icon in demo mode's under construction route

--- tracker/app/(demoRoutes)/under-construction/page.tsx
+++ tracker/app/(demoRoutes)/under-construction/page.tsx
@@ -1,9 +1,14 @@
+import { MdConstruction } from "react-icons/md";
 
 
 
 const UnderConstruction = () => {
   return (
-    <div>Under Construction</div>
+    <div className="h-96 w-full flex flex-col justify-center items-center gap-8">
+      <div className="capitalize text-xl font-serif font-semibold">demonstration mode is under construction</div>
+      <MdConstruction className="h-[30%] w-[30%] text-yellow-400"/>
+    </div>
+   
   )
 }
 export default UnderConstruction;
