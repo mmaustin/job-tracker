fbb632cdb4c49aec63969122a72c78e21421fd43
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-04 13:13:42 -0400

    Added create demo job action to demo actions file

---
+++ tracker/utils/demoActions.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use server';
+
+import DemoJob from "@/models/DemoJobs";
+import { connectToDB } from "./db";
+import { redirect } from "next/navigation";
+import dayjs from "dayjs";
+import { DemoJobType, CreateAndEditJobType, createAndEditJobSchema, DeletedQueryType } from './demoTypes';
+
+export async function createDemoJobAction(values: CreateAndEditJobType): Promise<DemoJobType | null | string> {
+
+  try {
+
+    createAndEditJobSchema.parse(values);
+
+
+    await connectToDB();
+    const job: DemoJobType = await DemoJob.create({
+      ...values
+    });
+
+    const jobStringified: string = JSON.stringify(job);
+
+    return jobStringified;
+  } catch (error) {
+    return null;
+  };
+};
