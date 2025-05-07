264e25d7be5faa9bfb0aa07fb67cb71347f0e30c
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-06 21:09:57 -0400

    Configured custom form select component and its prop for demo usage

--- tracker/components/demoComponents/DemoFormComponents.tsx
+++ tracker/components/demoComponents/DemoFormComponents.tsx
@@ -18,3 +18,27 @@ import {
 } from '@/components/ui/form';
 
 import { Input } from '../ui/input';
+
+type DemoCustomFormFieldProps = {
+  name: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: Control<any>;
+};
+
+export function CustomFormField({ name, control }: DemoCustomFormFieldProps) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className='capitalize'>{name}</FormLabel>
+          <FormControl>
+            <Input {...field} className='bg-background capitalize'/>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
