f85c9d93469fbf21621835b0df5c8bd8cdef4ec9
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-12 22:54:53 -0400

    Added jsx to demo job card component

--- tracker/components/demoComponents/DemoJobCard.tsx
+++ tracker/components/demoComponents/DemoJobCard.tsx
@@ -8,9 +8,31 @@ import { Badge } from '../ui/badge';
 import DemoJobInfo from './DemoJobInfo';
 import { MapPin, Briefcase, CalendarDays, RadioTower } from 'lucide-react';
 
-const DemoJobCard = () => {
+const DemoJobCard = ({job}: {job: DemoJobType}) => {
+  const date = new Date(job.createdAt).toLocaleDateString();
+
   return (
-    <div>DemoJobCard</div>
+    <Card className='bg-muted'>
+      <CardHeader>
+        <CardTitle >{job.position}</CardTitle>
+        <CardDescription >{job.company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent className='mt-4 grid grid-cols-2 gap-4 capitalize font-serif'>
+        <DemoJobInfo icon={<Briefcase />} text={job.mode}/>
+        <DemoJobInfo icon={<MapPin />} text={job.location}/>
+        <DemoJobInfo icon={<CalendarDays />} text={date}/>
+        <Badge className='w-24 sm:32 md:w-24 lg:w-32 justify-center' >
+          <DemoJobInfo icon={<RadioTower className='w-4 h-4 ' />} text={job.status}/>
+        </Badge>
+      </CardContent>
+      <CardFooter className='flex gap-4' >
+        <Button asChild size="sm" >
+          <Link href={`/jobs/${job._id}`} >edit</Link>
+        </Button>
+        <Button size="sm" >Delete </ Button>
+      </CardFooter>
+    </Card>
   )
 }
 export default DemoJobCard;
