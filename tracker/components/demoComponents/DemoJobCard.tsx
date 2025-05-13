2b96f70345c7d7291bcf13bc84c33f8a57129067
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-12 22:42:06 -0400

    Created demo job card component and added imports, minus delete job button

---
+++ tracker/components/demoComponents/DemoJobCard.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { DemoJobType } from '@/utils/demoTypes';
+import Link from 'next/link';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../ui/card';
+import { Separator } from '../ui/separator';
+import { Button } from '../ui/button';
+import { Badge } from '../ui/badge';
+import DemoJobInfo from './DemoJobInfo';
+import { MapPin, Briefcase, CalendarDays, RadioTower } from 'lucide-react';
+
+const DemoJobCard = () => {
+  return (
+    <div>DemoJobCard</div>
+  )
+}
+export default DemoJobCard;
