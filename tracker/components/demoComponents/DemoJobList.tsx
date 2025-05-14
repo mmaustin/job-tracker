32b38800c87c65ee09da81098aa1b209f740c229
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-14 12:48:57 -0400

    Created demo job list component, with imports

---
+++ tracker/components/demoComponents/DemoJobList.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+
+import { demoGetAllJobsAction } from "@/utils/demoActions";
+import { useQuery } from "@tanstack/react-query";
+import { DemoJobType } from '@/utils/demoTypes';
+import DemoJobCard from "./DemoJobCard";
+
+
+const DemoJobList = () => {
+  return (
+    <div>DemoJobList</div>
+  )
+}
+export default DemoJobList;
