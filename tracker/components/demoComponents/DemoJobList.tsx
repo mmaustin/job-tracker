d2d5d0d9bb33c58f38f67a6a1e1ccc7a34abe764
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-14 13:27:18 -0400

    Parsed data object returned from useQuery

--- tracker/components/demoComponents/DemoJobList.tsx
+++ tracker/components/demoComponents/DemoJobList.tsx
@@ -8,6 +8,29 @@ import DemoJobCard from "./DemoJobCard";
 
 
 const DemoJobList = () => {
+
+  const search = '';
+  const jobStatus = 'all';
+  const pageNumber = 1;
+
+  const {data, isPending} = useQuery({
+    queryKey: ['jobs', search, jobStatus, pageNumber],
+    queryFn: () => demoGetAllJobsAction({search, jobStatus, page: pageNumber}),
+  });
+
+  let parsedJobs: DemoJobType[] | DemoJobType | undefined | [];
+
+  if (data) {
+    parsedJobs = data.map(job => {
+      return JSON.parse(job);
+    });
+  } else {
+    parsedJobs = [];
+  };
+
+  if (isPending) return <h2 className="text-xl">Please Wait . . . </h2>
+  if (parsedJobs.length < 1) return <h2 className="text-xl font-semibold font-serif">No Jobs Found . . . </h2>
+
   return (
     <div>DemoJobList</div>
   )
