b5145aff6c557e2b71378271d5834990fb62490f
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-20 13:15:15 -0400

    Set up search params and handle submit function in demo search component

--- tracker/components/demoComponents/DemoSearchForm.tsx
+++ tracker/components/demoComponents/DemoSearchForm.tsx
@@ -7,6 +7,24 @@ import { JobStatus } from "@/utils/demoTypes";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 const DemoSearchForm = () => {
+
+  const searchParams = useSearchParams();
+  const jobStatus = searchParams.get('jobStatus') || 'all';
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget)
+    const jobStatus = formData.get('jobStatus') as string;
+    
+    const params = new URLSearchParams();
+    params.set('jobStatus', jobStatus);
+
+    router.push(`${pathname}?${params.toString()}`)
+  };
+
   return (
     <div>DemoSearchForm</div>
   )
