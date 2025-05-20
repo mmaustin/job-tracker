b10cc9ef9b0f1d8b0573ce80e78fa454facd1057
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-20 13:05:17 -0400

    Created demo search from component with imports

---
+++ tracker/components/demoComponents/DemoSearchForm.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Button } from "../ui/button";
+import { JobStatus } from "@/utils/demoTypes";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+
+const DemoSearchForm = () => {
+  return (
+    <div>DemoSearchForm</div>
+  )
+}
+export default DemoSearchForm
