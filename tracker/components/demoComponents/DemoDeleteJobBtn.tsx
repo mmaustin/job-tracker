4144c9590a8ce56caabe316c6b7daed2d5b643c2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-20 15:43:15 -0400

    Activated delete job action in react query's useMutation method, delete button component

--- tracker/components/demoComponents/DemoDeleteJobBtn.tsx
+++ tracker/components/demoComponents/DemoDeleteJobBtn.tsx
@@ -5,7 +5,28 @@ import { useToast } from "@/hooks/use-toast";
 import { demoDeleteJobAction } from "@/utils/demoActions";
 
 
-const DemoDeleteJobBtn = ({id}: {id: string}) => {
+const DemoDeleteJobBtn = ({ id }: { id: string }) => {
+
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (id: string) => demoDeleteJobAction(id),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({
+          description: 'there was an error',
+        });
+        return;
+      };
+      queryClient.invalidateQueries({ queryKey: ['jobs'] });
+      queryClient.invalidateQueries({ queryKey: ['stats'] });
+      queryClient.invalidateQueries({ queryKey: ['charts'] });
+
+      toast({ description: 'job removed' });
+    },
+  });
+
   return (
     <div>DemoDeleteJobBtn</div>
   )
