c0e75e157a2d6b435e8045e462820da34cc628e7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-22 15:22:47 -0400

    Created demo charts container compoent with imports

---
+++ tracker/components/demoComponents/DemoChartsContainer.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { demoGetChartsDataAction } from "@/utils/demoActions";
+import { useQuery } from '@tanstack/react-query';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from 'recharts';
+
+
+const DemoChartsContainer = () => {
+  return (
+    <div>DemoChartsContainer</div>
+  )
+}
+export default DemoChartsContainer
