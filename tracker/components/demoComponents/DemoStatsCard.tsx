3f04ebf6761917bc998971300c36bc1167878ef7
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2025-05-24 13:54:44 -0400

    Created a component to show a loading skeleton when stats data is loading

--- tracker/components/demoComponents/DemoStatsCard.tsx
+++ tracker/components/demoComponents/DemoStatsCard.tsx
@@ -6,9 +6,35 @@ type DemoStatsCardType = {
   value: number
 };
 
-const DemoStatsCard = ({title, value}: DemoStatsCardType) => {
+const DemoStatsCard = ({ title, value }: DemoStatsCardType) => {
   return (
-    <div>DemoStatsCard</div>
-  )
-}
+    <Card className="bg-muted">
+      <CardHeader className="flex flex-row justify-between items-center">
+        <CardTitle className="capitalize">{title}</CardTitle>
+        <CardDescription className="text-4l font-extrabold text-primary mt-[0px!important]">
+          {value}
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  );
+};
+
+
+export const DemoStatsLoadingCard = () => {
+  return (
+    <Card className='w-[330px] h-[88px]'>
+      <CardHeader className='flex flex-row justify-between items-center'>
+        <div className='flex items-center space-x-4'>
+          <Skeleton className='h-12 w-12 rounded-full' />
+          <div className='space-y-2'>
+            <Skeleton className='h-4 w-[150px]' />
+            <Skeleton className='h-4 w-[100px]' />
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  );
+};
+
+
 export default DemoStatsCard
